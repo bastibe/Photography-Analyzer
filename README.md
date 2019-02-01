c16089c79e57690061f3abe25858890420716f23
Bastian Bechtold <bb@mrbigglesworth.fritz.box>
2019-02-01 16:42:38 +0100

    adds README and screenshot

---
+++ README.md
@@ -0,0 +1,8 @@
+# Photography Analyzer
+
+1. `collect_data.py` crawls all your pictures, and extracts image metadata such as the camera, the lens, the focal length, and the rating.
+2. `analyze_data.py` reads all that collected data, and creates a wonderful analysis of it:
+
+![screenshot](https://raw.githubusercontent.com/bastibe/Photography-Analyzer/master/analysis.png)
+
+This code is public domain.
--- collect_data.py
+++ collect_data.py
@@ -4,9 +4,12 @@ from glob import glob
 from tqdm import tqdm
 from datetime import datetime
 
+# where to search for pictures:
 root = '/home/bb/Pictures/'
 
 def get_exif(filename):
+    """Call exiftool for a filename, and extract EXIF data."""
+
     important_keys = ['Exposure Time', 'Rating', 'F Number', 'Lens',
                       'ISO', 'Focal Length', 'Camera Model Name', 'Create Date']
 
@@ -14,6 +17,7 @@ def get_exif(filename):
     exif = {}
     exif['Project'] = filename.split('/')[3]
     exif['Filename'] = filename.split('/')[-1]
+    # parse exiftool output:
     try:
         for line in proc.stdout.splitlines():
             key, value = (x.strip() for x in line.decode().split(':', 1))
@@ -40,14 +44,16 @@ def get_exif(filename):
         print(filename)
     return exif
 
+# search for these file names:
 files = glob(root + '**/*.jpg') + glob(root + '**/*.JPG') + glob(root + '**/*.jpeg')
 from multiprocessing import Pool
 
 if __name__ == '__main__':
-    get_exif('/home/bb/Pictures/2005-2006 Zivildienst/Schullandheim 157.jpg')
     metadata = []
+    # run with 16 processes in parallel:
     with Pool(16) as p:
         metadata = list(tqdm(p.imap_unordered(get_exif, files), total=len(files), smoothing=0))
 
+# save as pickled dataframe:
 df = DataFrame(metadata)
 df.to_pickle('metadata.pkl')
